26f642147641be70865e9c8e9db1754e8d60b4fa
mk-saadi <mksaadi820@gmail.com>
2024-05-18 17:46:24 +0600

    entry point name changed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
 	mode: "production",
-	entry: "./src/Toast.jsx",
+	entry: "./src/ToastProvider.jsx",
 	output: {
 		path: path.resolve("build"),
 		filename: "index.js",
